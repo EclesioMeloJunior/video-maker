f5e19d40881eeb79b9f0537efa757d9372dbeb2c
eclesiomelojunior <eclesiomelo.1@gmail.com>
2019-06-23 19:54:52 -0400

    feature: add input robot and state manager robot

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-const readline = require("readline-sync");
 const robots = {
+	input: require("./robots/input.js"),
 	text: require("./robots/text.js")
 };
 
 async function start() {
-	const content = {
-		maximumSentences: 7
-	};
-
-	content.searchTerm = askAndReturnSearchTerm();
-	content.prefix = askAndReturnPrefix();
-
-	await robots.text(content);
-
-	function askAndReturnSearchTerm() {
-		return readline.question("Type a Wikipedia search term: ");
-	}
-
-	function askAndReturnPrefix() {
-		const prefixes = ["Who is", "What is", "The history of"];
-		const selectedPrefixIndex = readline.keyInSelect(
-			prefixes,
-			"Choose one option:"
-		);
-		const selectedPrefixText = prefixes[selectedPrefixIndex];
-		return selectedPrefixText;
-	}
-
-	console.log(JSON.stringify(content, null, 4));
+	robots.input();
+	await robots.text();
 }
 
 start();
