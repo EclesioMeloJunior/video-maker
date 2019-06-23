89bef48c0ec805ba07936f1f4c8c6482ea21725a
eclesiomelojunior <eclesiomelo.1@gmail.com>
2019-06-23 19:28:34 -0400

    feature: add watson natural language understanding

--- robots/text.js
+++ robots/text.js
@@ -1,7 +1,8 @@
 const algorithmia = require("algorithmia");
 const algorithmiaApiKey = require("../credentials/algorithmia.json").apiKey;
+
 const sentenceBoundaryDetection = require("sbd");
-const watsonApiKey = require("../credentials/watson-nlu.json").apiKey;
+const watsonApiKey = require("../credentials/watson-nlu.json").apikey;
 
 const NaturalLanguageUnderstantingV1 = require("watson-developer-cloud/natural-language-understanding/v1.js");
 
@@ -15,6 +16,8 @@ async function robot(content) {
 	await fetchContentFromWikipedia(content);
 	sanitizeContent(content);
 	breakContentIntoSetences(content);
+	limitMaximumSentences(content);
+	await fetchKeywordsOfAllSentences(content);
 
 	async function fetchContentFromWikipedia(content) {
 		const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
@@ -69,8 +72,40 @@ async function robot(content) {
 				images: []
 			});
 		});
+	}
 
-		console.log(content);
+	function limitMaximumSentences(content) {
+		content.sentences = content.sentences.slice(0, content.maximumSentences);
+	}
+
+	async function fetchKeywordsOfAllSentences(content) {
+		for (const sentence of content.sentences) {
+			sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text);
+		}
+	}
+
+	async function fetchWatsonAndReturnKeywords(sentence) {
+		return new Promise((resolve, reject) => {
+			nlu.analyze(
+				{
+					text: sentence,
+					features: {
+						keywords: {}
+					}
+				},
+				(error, response) => {
+					if (error) {
+						throw error;
+					}
+
+					const keywords = response.keywords.map(keyword => {
+						return keyword.text;
+					});
+
+					resolve(keywords);
+				}
+			);
+		});
 	}
 }
 
